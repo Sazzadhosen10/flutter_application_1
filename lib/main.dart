2a9cc576f98b0196f6bb0b463d7a9c061d30c654
Sazzadhosen10 <hosen15-5836@diu.edu.bd>
2025-02-16 11:04:25 +0600

    add

--- lib/main.dart
+++ lib/main.dart
@@ -10,49 +10,65 @@ class FirstUI extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      //title: "This is Sazzad",
       home: Scaffold(
-          appBar: AppBar(
-            title: Text("Basic Flutter UI"),
-          ),
-          body: Column(
-            children: [
-              const Text(
-                "4.0",
-                style: TextStyle(
-                    color: Color.fromARGB(255, 50, 46, 46), fontSize: 30),
+        appBar: AppBar(
+          title: const Text("Basic Flutter UI"),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+              "4.0",
+              style: TextStyle(
+                  color: Color.fromARGB(255, 50, 46, 46),
+                  fontSize: 30,
               ),
-              Row(
-                children: const [
-                  Icon(
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Expanded(
+                  child: Icon(
                     Icons.star,
                     size: 50,
                     color: Colors.black,
                   ),
-                  Icon(
+                ),
+                const Flexible(
+                  fit: FlexFit.loose,
+                  flex: 3,
+                  child: Icon(
                     Icons.star,
                     size: 50,
                     color: Colors.black,
                   ),
-                  Icon(
-                    Icons.star,
-                    size: 50,
-                    color: Colors.black,
-                  ),
-                  Icon(
-                    Icons.star,
-                    size: 50,
-                    color: Colors.black,
-                  ),
-                  Icon(
-                    Icons.star_border,
-                    size: 50,
-                    color: Color.fromARGB(255, 19, 18, 18),
-                  ),
-                ],
-              )
-            ],
-          )),
+                ),
+                const Icon(
+                  Icons.star,
+                  size: 50,
+                  color: Colors.black,
+                ),
+                const Icon(
+                  Icons.star,
+                  size: 50,
+                  color: Colors.black,
+                ),
+                const Icon(
+                  Icons.star_border,
+                  size: 50,
+                  color: Color.fromARGB(255, 19, 18, 18),
+                ),
+                const Text(
+                  "submit",
+                  style: TextStyle(fontSize: 20),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
