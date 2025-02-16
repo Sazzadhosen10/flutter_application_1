456db98384727e26b05b994144de401d620538bb
Sazzadhosen10 <hosen15-5836@diu.edu.bd>
2025-02-16 09:23:18 +0600

    initiate

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FirstUI());
+}
+
+class FirstUI extends StatelessWidget {
+  const FirstUI({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //title: "This is Sazzad",
+      home: Scaffold(
+        appBar: AppBar(
+          title: "This is Sazzad",
+          backgroundColor: Colors.red,
+          foregroundColor: Colors.blue,
+          centerTitle: true,
+          elevation: 10.5,
+          title: Text("Basic Flutter UI"),
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          margin: EdgeInsets.all(35),
+          padding: EdgeInsets.all(35),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(50),
+            gradient: LinearGradient(colors: [
+
+            ])
+          ),
+        ),
+      ),
+    );
+  }
+}
